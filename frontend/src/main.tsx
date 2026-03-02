c5616ee9f7690aeb52700630c149c5615603d829
yours-truly68 <mohammedrazim880@gmail.com>
2026-03-03 03:56:34 +0530

    feat(login): enhance login page with Google login button and update UI elements

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-export const authService = "http://localhost:5000";
+export const authService = "http://localhost:8000";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -4,6 +4,7 @@ import { authService } from "../main";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { useGoogleLogin } from "@react-oauth/google";
+import { FcGoogle } from "react-icons/fc";
 
 export const Login = () => {
   const [loading, setLoading] = useState<boolean>(false);
@@ -36,7 +37,27 @@ export const Login = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-white px-4">
       <div className="w-full max-w-sm space-y-6">
-        <h1 className="text-center text-3xl font-bold text-[#e23774]"></h1>
+        <h1 className="text-center text-3xl font-bold text-[#e23774]">
+          Zomato-Clone
+        </h1>
+        <p className="text-center text-sm text-gray-500">
+          Login or Sign Up to Continue
+        </p>
+        <button
+          className="flex w-full items-center justify-center gap-3 rounded-xl border border-gray-300 bg-white px-4 py-4"
+          onClick={googleLogin}
+          disabled={loading}
+        >
+          <FcGoogle size={20} />
+          {loading ? "Signing in..." : "Continue with Google"}
+        </button>
+        <p className="text-center text-xs text-gray-400">
+          By continuing, you agree with our{" "}
+          <span className="text-[#e23774]">Terms & Conditions</span> &{" "}
+          <span className="text-[#e23774] w-full inline-block">
+            Privacy Policy
+          </span>
+        </p>
       </div>
     </div>
   );
