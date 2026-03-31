0aaf3f3b991114fcdcb09c7e30bdc1371136fb1c
yours-truly68 <mohammedrazim880@gmail.com>
2026-03-31 21:36:06 +0530

    feat: implement payment consumer for handling payment success events and update order status

---
+++ services/restaurant/src/config/payment.consumer.ts
@@ -0,0 +1,60 @@
+import Order from "../models/Orders.model.js";
+import { getChannel } from "./rabbitmq.config.js";
+
+export const startPaymentConsumer = async () => {
+  const channel = getChannel();
+  if (!channel) {
+    console.error("🐇 RabbitMQ channel is not available");
+    return;
+  }
+
+  channel.consume(process.env.PAYMENT_QUEUE!, async (msg) => {
+    if (!msg) return;
+    try {
+      const event = JSON.parse(msg.content.toString());
+      if (event.type !== "PAYMENT_SUCCESS") {
+        channel.ack(msg);
+        return;
+      }
+
+      const { orderId } = event.data;
+
+      const order = await Order.findOneAndUpdate(
+        {
+          _id: orderId,
+          paymentStatus: { $ne: "paid" },
+        },
+        {
+          $set: {
+            paymentStatus: "paid",
+            status: "placed",
+          },
+          $unset: {
+            expiresAt: 1,
+          },
+        },
+        {
+          new: true,
+        },
+      );
+
+      if (!order) {
+        channel.ack(msg);
+        return;
+      }
+
+      console.log("✅ Payment successful for order:", order._id);
+
+
+      //socket work
+
+
+      channel.ack(msg);
+    } catch (error) {
+      console.error(
+        "🐇 Error occurred while consuming payment messages:\n",
+        error,
+      );
+    }
+  });
+};
