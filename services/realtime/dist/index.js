be7264a1c3ecf2f0100e7d6802447a6059ab89d9
yours-truly68 <mohammedrazim880@gmail.com>
2026-04-08 04:13:24 +0530

    feat: implement internal routes for event emission with validation and logging

--- services/realtime/dist/index.js
+++ services/realtime/dist/index.js
@@ -3,14 +3,16 @@ import { config } from "dotenv";
 import cors from "cors";
 import { createServer } from "http";
 import { initSocket } from "./socket.js";
+import internalRoutes from "./routes/internal.routes.js";
 config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use("/api/v1/internal", internalRoutes);
 const server = createServer(app);
 initSocket(server);
 const PORT = process.env.PORT || 8003;
 server.listen(PORT, () => {
-    console.log(`Realtime is listenting on port: ${PORT}`);
+    console.log(`Realtime is listening on port: ${PORT}`);
 });
---
+++ services/realtime/src/routes/internal.routes.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import { getIO } from "../socket.js";
+
+const router = express.Router();
+
+router.post("/emit", (req, res) => {
+  if (
+    req.headers["x-internal-service-key"] !== process.env.INTERNAL_SERVICE_KEY
+  ) {
+    return res
+      .status(403)
+      .json({ message: "Forbidden - Invalid internal service key" });
+  }
+  // Handle the emit logic here
+
+  const { event, room, payload } = req.body;
+
+  if (!event || !room) {
+    return res.status(400).json({ message: "Event and room are required" });
+  }
+
+  const io = getIO();
+
+  console.log(
+    `Emitting event: ${event} to room: ${room} with payload:`,
+    payload,
+  );
+
+  io.to(room).emit(event, payload ?? { message: "No payload provided" });
+
+  return res.json({
+    success: true,
+    message: `Event ${event} emitted to room ${room}`,
+  });
+});
+
+export default router;
