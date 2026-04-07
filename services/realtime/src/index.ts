460a9b2e034095b4a7df12df8a17fdb54439f270
yours-truly68 <mohammedrazim880@gmail.com>
2026-04-07 05:39:18 +0530

    feat: implement socket.io server with authentication and user management

---
+++ services/realtime/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import { createServer } from "http";
+import { initSocket } from "./socket.js";
+
+config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+const server = createServer(app);
+
+initSocket(server);
+
+const PORT = process.env.PORT || 8003;
+app.listen(PORT, () => {
+  console.log(`Realtime is listenting on port: ${PORT}`);
+});
---
+++ services/realtime/src/socket.ts
@@ -0,0 +1,73 @@
+import { Server } from "socket.io";
+import http from "http";
+import jwt from "jsonwebtoken";
+
+let io: Server;
+
+export const initSocket = (server: http.Server) => {
+  io = new Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.use((socket, next) => {
+    try {
+      const token = socket.handshake.auth?.token;
+
+      if (!token) {
+        return next(new Error("Authentication error: No token provided"));
+      }
+      const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET! as string,
+      ) as any;
+
+      if (!decoded || !decoded.user) {
+        return next(new Error("Authentication error: Invalid token"));
+      }
+
+      // Attach user info to socket object for later use
+      socket.data.user = decoded.user;
+
+      next();
+    } catch (error) {
+      console.error("Socket authentication error:", error);
+      next(new Error("Authentication error"));
+    }
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.data.user.name}`);
+    const user = socket.data.user;
+
+    if (!user) {
+      socket.disconnect();
+      return;
+    }
+
+    const userId = user._id;
+    socket.join(`user_${userId}`);
+
+    if (user.restaurantId) {
+      socket.join(`restaurant_${user.restaurantId}`);
+    }
+
+    console.log(`User ${user.name}}`);
+    console.log(`Socket: `, [...socket.rooms]);
+
+    socket.on("disconnect", () => {
+      console.log(`User disconnected: ${socket.data.user.name}`);
+    });
+  });
+
+  console.log("Socket.io initialized");
+  return io;
+};
+
+export const getIO = () => {
+  if (!io) {
+    throw new Error("Socket.io not initialized");
+  }
+  return io;
+};
