1347d27b4b9009117e8b17695fd4394e07bff424
yours-truly68 <mohammedrazim880@gmail.com>
2026-04-10 06:51:58 +0530

    feat: enhance SocketProvider with improved connection handling and integrate realtimeService

--- frontend/src/context/SocketContext.tsx
+++ frontend/src/context/SocketContext.tsx
@@ -9,6 +9,7 @@ import {
 
 import { io, Socket } from "socket.io-client";
 import { useAppData } from "./AppContext";
+import { realtimeService } from "../main";
 
 interface SocketContextType {
   socket: Socket | null;
@@ -16,15 +17,57 @@ interface SocketContextType {
 
 const SocketContext = createContext<SocketContextType>({ socket: null });
 
-const SocketProvider = ({ children }: { children: ReactNode }) => {
+export const SocketProvider = ({ children }: { children: ReactNode }) => {
   const { isAuth } = useAppData();
 
   const socketRef = useRef<Socket | null>(null);
+  const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
     if (!isAuth) {
       socketRef.current?.disconnect();
       socketRef.current = null;
+      return;
     }
-  });
+
+    if (socketRef.current) return;
+
+    const token = localStorage.getItem("token");
+    if (!token) return;
+
+    const socket = io(realtimeService, {
+      auth: {
+        token,
+      },
+      transports: ["websocket"],
+    });
+    socketRef.current = socket;
+    setSocket(socket);
+
+    socket.on("connect", () => {
+      console.log("Connected to WebSocket server: ", socket.id);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from WebSocket server: ", socket.id);
+    });
+
+    socket.on("connect_error", (err) => {
+      console.error("WebSocket connection error: ", err.message);
+    });
+
+    return () => {
+      socket.disconnect();
+      socketRef.current = null;
+      setSocket(null);
+    };
+  }, [isAuth]);
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
+
+export const useSocket = () => useContext(SocketContext);
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AppProvider } from "./context/AppContext.tsx";
 import "leaflet/dist/leaflet.css";
+import { SocketProvider } from "./context/SocketContext.tsx";
 
 export const authService = "http://localhost:8000";
 export const restaurantService = "http://localhost:8001";
@@ -15,7 +16,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="1037277292337-nbmi3ubrt032rf1h023d6todvp5os08n.apps.googleusercontent.com">
       <AppProvider>
-        <App />
+        <SocketProvider>
+          <App />
+        </SocketProvider>
       </AppProvider>
     </GoogleOAuthProvider>
   </StrictMode>,
