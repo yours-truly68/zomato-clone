82424c2e6fd1e09531e821f0fe5169697b85f496
yours-truly68 <mohammedrazim880@gmail.com>
2026-04-18 15:56:48 +0530

    changed Auth index file

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -7,14 +7,23 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://zomato-clone-1d3p.vercel.app"],
     credentials: true,
   }),
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(express.json());
 app.use("/api/auth", authRoute);
+
+app.options("*", cors());
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
